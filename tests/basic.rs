2fbdc90c57f4dd9a584fa31f21391d49f915b8c7
pokermonb <pokermonb@outlook.com>
2025-03-22 15:27:31 +0800

    Update basic.rs

--- tests/basic.rs
+++ tests/basic.rs
@@ -5,36 +5,6 @@ use pebbles_game_io::*;
 const USER: u64 = 555;
 
 #[test]
-fn test_game_initialization() {
-    let sys = System::new();
-
-    sys.init_logger();
-
-    let program = Program::from_file(
-        &sys,
-        "./target/wasm32-unknown-unknown/debug/pebbles_game.opt.wasm",
-    );
-
-    sys.mint_to(USER, 500000000000000000);
-
-    let pebbles_init: PebblesInit = PebblesInit {
-        pebbles_count: 10,
-        max_pebbles_per_turn: 3,
-        difficulty: DifficultyLevel::Easy,
-    };
-
-    let _res = program.send_bytes(USER, pebbles_init.encode());
-    sys.run_next_block();
-
-    let game: GameState = program.read_state(()).expect("Failed to read state");
-
-    assert_eq!(game.pebbles_count, 10);
-    assert_eq!(game.max_pebbles_per_turn, pebbles_init.max_pebbles_per_turn);
-    assert_eq!(game.pebbles_remaining, 10);
-    assert!(game.winner.is_none());
-}
-
-#[test]
 fn test_player_turn() {
     let system = System::new();
     let program = Program::current(&system);
